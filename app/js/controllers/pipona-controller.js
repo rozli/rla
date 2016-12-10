50579acf3370bbeb3fad902e89b763c8fee01834
rozli <rozito@gmail.com>
2016-12-10 08:52:51 +0200

    FB share button added to all product pages, refactoring of home-controller.js and app.js, css improvements

--- app/js/controllers/pipona-controller.js
+++ app/js/controllers/pipona-controller.js
@@ -1,14 +1,19 @@
 (function () {
   'use strict';
 
-  function PiponaController($rootScope, $scope, PageTitle) {
+  function PiponaController($rootScope, $scope, $location, PageTitle) {
     var vm = this;
     PageTitle.setTitle('RozLi - Приказната вила Пипона');
 
     rozLiJs.initialize();
     rozLiJs.initializeGallery("pipona");
+
+    // FB share params
+    $scope.fbShareURL = $rootScope.texts.url + $location.path();
+    $scope.fbShareMedia = $rootScope.texts.url + '/images/p_chair_pipona_1.jpg';
+    $scope.fbShareDesc = $rootScope.texts.fbStart + $rootScope.texts.piponaHeading;
   }
 
   angular.module('rozLi.controllers')
-    .controller('PiponaController', ['$rootScope', '$scope', 'PageTitle', PiponaController]);
+    .controller('PiponaController', ['$rootScope', '$scope', '$location', 'PageTitle', PiponaController]);
 } ());
